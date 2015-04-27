e53525b05ae91ca9708a9a25e7ebb550486b54a1
nstankovsky <nstankovsky@gmail.com>
2015-04-27 13:28:00 +0300

    Initial commit

---
+++ desktop/src/com/liperim/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.liperim.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.liperim.demo.GameCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameCore(), config);
+	}
+}
