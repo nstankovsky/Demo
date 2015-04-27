33633fbbdfb632315b87b741bacc5144392fc5bc
nstankovsky <nstankovsky@gmail.com>
2015-04-27 13:38:44 +0300

    new changes

--- desktop/src/com/liperim/demo/desktop/DesktopLauncher.java
+++ desktop/src/com/liperim/demo/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.liperim.demo.GameCore;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 1024;
+		config.height = 600;
+		config.resizable = false;
 		new LwjglApplication(new GameCore(), config);
 	}
 }
